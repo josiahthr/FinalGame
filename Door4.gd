5dd26398e56b267680cd50d833a1c0f72be46097
unknown <jreigner5040@stevenscollege.edu>
2025-04-16 17:16:19 -0400

    please work

--- Door4.gd
+++ Door4.gd
@@ -1,10 +1,10 @@
 extends StaticBody3D
 
 
-@export var interaction_text: String = "I4DoorForward"
+@export var interaction_text: String = "I5DoorForward"
 
 func interact():
-	print("interacted with I4DoorForward")
+	print("interacted with I5DoorForward")
 	get_tree().change_scene_to_file("res://I5.tscn")
 
 func get_interaction_text():
