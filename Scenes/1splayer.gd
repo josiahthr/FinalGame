9b38a4fb659ee9bc44726c2a999fb2f9fb716bc8
unknown <jreigner5040@stevenscollege.edu>
2025-04-19 21:47:00 -0400

    reset mechanic

---
+++ Scenes/1splayer.gd
@@ -0,0 +1,59 @@
+extends CharacterBody3D
+
+
+var SPEED = 10.0
+const JUMP_VELOCITY = 10
+
+@export var mouse_sensitivity: float = 0.002
+var in_dialogue = false
+var current_target = null
+
+@onready var neck := $Neck
+@onready var camera := $Neck/Camera
+@onready var _dialog : Control = $"../CanvasLayer/Dialog"
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			neck.rotate_y(-event.relative.x * mouse_sensitivity)
+			camera.rotate_x(-event.relative.y * mouse_sensitivity)
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(40))
+
+
+
+func _physics_process(delta: float) -> void:
+			
+			
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	#[wave], [shake], [color]
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+
+func _on_static_body_3d_child_entered_tree(node: Node) -> void:
+	pass
+
+
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	print("aiosjrbnoiquaJnf")
+	
+func move_to_position(target_position: Vector3):
+	global_position = target_position
---
+++ Scenes/badobstacle.gd
@@ -0,0 +1,17 @@
+extends PathFollow3D
+
+@export var reset_position: Vector3 = Vector3(0, 0, 43)
+@onready var collision_area = get_node("MeshInstance3D/Area3D")
+
+func _ready():
+	if collision_area:
+		collision_area.body_entered.connect(_on_area_body_entered)
+	else:
+		printerr(": Area3D node not found!")
+
+func _on_area_body_entered(body):
+	if body.name == "Character":
+		body.move_to_position(reset_position)
+
+func _process(delta):
+	progress += 15 * delta
