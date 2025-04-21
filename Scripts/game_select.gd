22117ecdc32b8de4f097315ee11773ac51b9ac80
unknown <jreigner5040@stevenscollege.edu>
2025-04-20 22:46:43 -0400

    audio and menu changes

--- Scripts/game_select.gd
+++ Scripts/game_select.gd
@@ -9,3 +9,7 @@ func _on_button_2_pressed() -> void:
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Level.tscn")
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
--- Scripts/titlescreen.gd
+++ Scripts/titlescreen.gd
@@ -8,3 +8,8 @@ func _on_start_pressed() -> void:
 
 func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Settings.tscn")
+
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().quit()
