b14860fdbf2c0cdb5e347f4787ebe4b46aaead8b
josiahthr <josiahtr>
2025-04-23 12:50:39 -0400

    added interaction buttons

--- Objects/silent_hill_1_meshes_-_entire_normal_school/textures/map.gd
+++ Objects/silent_hill_1_meshes_-_entire_normal_school/textures/map.gd
@@ -1,8 +1,11 @@
 extends StaticBody3D
 
 @export var interaction_text: String = "SchoolMap"
+@onready var _map_panel : Panel = $"../../../../../../CanvasLayer/Dialog/Map" 
+@onready var yes_button := $"../../../../../../CanvasLayer/Dialog/Yes"
+@onready var no_button := $"../../../../../../CanvasLayer/Dialog/Button2"
 @export var dialogue_line: Array[String] = [
-	"There is a [color=#30bf2e]School Map[/color].
+	"There is a [color=#0cebc2]School Map[/color].
 	[left]         Take it?[/left]"
 ]
 @export var speaker_name: String = " " 
@@ -14,6 +17,9 @@ func interact():
 	
 func get_dialogue_data():
 	if dialogue_index < dialogue_line.size():
+		_map_panel.visible = true 
+		yes_button.visible = true
+		no_button.visible = true
 		var line = dialogue_line[dialogue_index]
 		dialogue_index += 1
 		return {
@@ -22,6 +28,9 @@ func get_dialogue_data():
 		}
 	else:
 		dialogue_index = 0
+		_map_panel.visible = false 
+		yes_button.visible = false
+		no_button.visible = false
 		return null
 
 func get_interaction_text():
--- Objects/silent_hill_1_meshes_-_entire_normal_school/textures/signinteract.gd
+++ Objects/silent_hill_1_meshes_-_entire_normal_school/textures/signinteract.gd
@@ -2,7 +2,7 @@ extends StaticBody3D
 
 @export var interaction_text: String = "MidwichSign"
 @export var dialogue_line: Array[String] = [
-	"Just a wall."
+	"[color=#0cebc2]Midwich Elementary School[/color]"
 ]
 @export var speaker_name: String = " " 
 
