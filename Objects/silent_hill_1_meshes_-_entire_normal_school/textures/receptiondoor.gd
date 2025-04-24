a87ed5c6bdc4c3166bfefc669f1c08fc26daf46a
unknown <jreigner5040@stevenscollege.edu>
2025-04-24 18:41:47 -0400

    smoother door mechanics

--- Objects/silent_hill_1_meshes_-_entire_normal_school/textures/receptiondoor.gd
+++ Objects/silent_hill_1_meshes_-_entire_normal_school/textures/receptiondoor.gd
@@ -2,14 +2,19 @@ extends StaticBody3D
 
 var is_tilted: bool = false
 @export var tilt_angle_degrees: float = -85.0
+@export var tilt_duration: float = 2
 @onready var door :=  $".."
-
+var tween: Tween
 
 func interact():
 	print("interacted with MidwichSign")
+	if tween and tween.is_running():
+		tween.kill()
+	
+	tween = create_tween()
 	if not is_tilted:
-		door.rotation_degrees.y += tilt_angle_degrees
+		tween.tween_property(door, "rotation_degrees:y", tilt_angle_degrees, tilt_duration)
 		is_tilted = true
 	else:
-		door.rotation_degrees.y -= tilt_angle_degrees
+		tween.tween_property(door, "rotation_degrees:y", 0.0, tilt_duration)
 		is_tilted = false
