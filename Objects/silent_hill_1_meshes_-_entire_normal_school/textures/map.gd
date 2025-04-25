a2c97e2e8e528c355a2a82c92c4b92c54a7d416f
unknown <jreigner5040@stevenscollege.edu>
2025-04-24 21:21:40 -0400

    uninteractable map after pickup

--- Objects/silent_hill_1_meshes_-_entire_normal_school/textures/map.gd
+++ Objects/silent_hill_1_meshes_-_entire_normal_school/textures/map.gd
@@ -5,6 +5,7 @@ signal map_chosen(picked_up: bool)
 @export var interaction_text: String = "SchoolMap"
 @onready var _map_panel : Panel = $"../../../../../../CanvasLayer/Dialog/Map"
 @onready var yes_button := $"../../../../../../CanvasLayer/Dialog/Yes"
+@onready var mapcol := $".."
 @onready var no_button := $"../../../../../../CanvasLayer/Dialog/Button2"
 @onready var text := $"../../../../../../CanvasLayer/Dialog/VBoxContainer/Dialogue"
 @onready var mapgone := $"../../../../../../mapgone"
@@ -32,13 +33,14 @@ var dialogue_index := 0
 var can_interact := true
 
 func interact():
-	if can_interact:
+	if can_interact == true:
 		print("interacted with SchoolMap")
 		_map_panel.visible = true
 		yes_button.visible = true
 		no_button.visible = true
 		yes_button.grab_focus()
 		can_interact = false
+
 
 func get_dialogue_data():
 	if dialogue_index < dialogue_line.size():
@@ -69,6 +71,7 @@ func _on_yes_pressed() -> void:
 	#"I was chosen by God because I am the best programmer 
 	#on the planet and God boosted my IQ with divine intellect." -Terry A. Davis
 	mapgone.show()
+	mapcol.queue_free()
 
 
 func _on_button_2_pressed() -> void:
