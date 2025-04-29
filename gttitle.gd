18d16051288dd0a8f5878a05944373e1df5786de
josiahthr <josiahtr>
2025-04-29 15:20:35 -0400

    arcade menu

---
+++ gttitle.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+
+
+func _on_quick_arcade_pressed() -> void:
+	get_tree().change_scene_to_file("res://Arcade.tscn")
+	
+
+func _on_simulation_mode_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_replay_theater_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
