156a18fab2ef8260d6256bea1b4cf796a813e203
josiahthr <josiahtr>
2025-05-01 14:35:00 -0400

    2 car selection

--- NissanSelect.gd
+++ NissanSelect.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
 @onready var nissanzxmod: Node3D = $"300ZX"
-@onready var nissanzxlab: Control = $Control
+@onready var nissanzxlab: Control = $"300zxLabels"
+@onready var skylinemod: Node3D = $Skyline
+@onready var skylinelab: Control = $SkylineLabels
 var nissanzx = true
+var skyline = false
 
 func _physics_process(delta: float) -> void:
 	if nissanzx == true:
@@ -11,7 +14,18 @@ func _physics_process(delta: float) -> void:
 	elif nissanzx == false:
 		nissanzxmod.hide()
 		nissanzxlab.hide()
+	if skyline == true:
+		skylinemod.show()
+		skylinelab.show()
+	elif skyline == false:
+		skylinemod.hide()
+		skylinelab.hide()
 
 
 func _on_button_2_pressed() -> void:
 	nissanzx = false
+	skyline = true
+
+func _on_button_3_pressed() -> void:
+	nissanzx = true
+	skyline = false
