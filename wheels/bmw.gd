7b228f9af952b36cf5ef65ad05d14a634c76faa3
unknown <jreigner5040@stevenscollege.edu>
2025-05-02 23:47:56 -0400

    driveable E30

--- wheels/bmw.gd
+++ wheels/bmw.gd
@@ -22,3 +22,10 @@ func _on_button_pressed() -> void:
 
 func _on_button_10_pressed() -> void:
 	get_tree().change_scene_to_file("res://ManuSelect.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	if current_car == 0:
+		print("we should be goin")
+		CarSelection.selected_car_scene = "res://E30.tscn"
+		get_tree().change_scene_to_file("res://hyundai_pony_ps1_style/cartest.tscn")
