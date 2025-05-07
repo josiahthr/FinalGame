b70c73a666fee4b925da85d566025781ff741392
josiahthr <josiahtr>
2025-05-07 15:16:10 -0400

    added navigation logic

---
+++ silent_hill_1_monsters_-_groaner/Navigation.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+@onready var navigation: NavigationAgent3D = $NavigationAgent3D
+
+@export var speed: float = 3.0
+
+@onready var player: CharacterBody3D = get_node("../../../Player") 
+
+func _physics_process(delta: float) -> void:
+	if player and navigation:
+		navigation.set_target_position(player.global_transform.origin)
+		var next_position = navigation.get_next_path_position()
+		var direction = (next_position - global_transform.origin).normalized()
+		if direction.length() > 0.1:
+			look_at(next_position, Vector3.UP)
+		global_translate(direction * speed * delta)
